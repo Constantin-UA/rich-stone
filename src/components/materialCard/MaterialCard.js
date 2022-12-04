9b5076da53d84e00ff4d59622c6b16b90946e615
Constantin-UA <konstantinsh579@gmail.com>
2022-12-04 21:59:23 +0200

    fix materials

--- src/components/materialCard/MaterialCard.js
+++ src/components/materialCard/MaterialCard.js
@@ -12,12 +12,12 @@ function MaterialCard({
 			<img className="card-img" src={cardImg} alt="bg" />
 			<div className="card-body">
 				<div className="card-title">{cardTitle}</div>
-				<div className="card-text">
+				{/* 				<div className="card-text">
 					<p>{cardDescr.V}</p>
 					<p>{cardDescr.M}</p>
 					<p>{cardDescr.Z}</p>
 					<p>{cardDescr.W}</p>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
--- src/components/materials/Materials.js
+++ src/components/materials/Materials.js
@@ -3,24 +3,16 @@ import Image from 'react-bootstrap/Image';
 import { useEffect } from 'react';
 import MaterialCard from '../materialCard/MaterialCard';
 //images imports
-import matImg_0 from '../../assets/materials/granit-1.jpg';
-import matImg_1 from '../../assets/materials/granit-2.jpg';
-import matImg_2 from '../../assets/materials/granit-3.jpg';
-import matImg_3 from '../../assets/materials/granit-3.jpg';
-import matImg_4 from '../../assets/materials/granit-3.jpg';
+import matImg_0 from '../../assets/materials/Granit-1.png';
+import matImg_1 from '../../assets/materials/Marmor-1.png';
+import matImg_2 from '../../assets/materials/Aqril-1.png';
+import matImg_3 from '../../assets/materials/Quarz-1.png';
 
 //scss imports
 import './materials.scss';
-const dataImg = [matImg_0, matImg_1, matImg_2, matImg_3, matImg_4, matImg_1, matImg_2]; //
+const dataImg = [matImg_0, matImg_1, matImg_2, matImg_3]; //
 const cardsData = {
-	dataTitle: [
-		'Ukrainian Autumn',
-		'Rosso Toledo',
-		'Black Sea',
-		'Ukrainian Autumn',
-		'Rosso Toledo',
-		'Black Sea',
-	],
+	dataTitle: ['Граніт', 'Мармур', 'Акрил', 'Кварц'],
 	dataDescr: [
 		{
 			V: "Об'ємна вага 2680 кг/м3",
@@ -45,24 +37,6 @@ const cardsData = {
 			M: 'Міцність при стисканні 240 МРа',
 			Z: 'Зносостійкість 0,3 – 2,5 г/см',
 			W: 'Водопоглинання 0,15 - 1,02%',
-		},
-		{
-			V: "Об'ємна вага 2800 кг/м3",
-			M: 'Міцність при стисканні 142 МРа',
-			Z: 'Зносостійкість 0,60 г/см',
-			W: 'Водопоглинання 0,21%',
-		},
-		{
-			V: "Об'ємна вага 2600 – 2640 кг/м3",
-			M: 'Міцність при стисканні 240 МРа',
-			Z: 'Зносостійкість 0,3 – 2,5 г/см',
-			W: 'Водопоглинання 0,15 - 1,02%',
-		},
-		{
-			V: "Об'ємна вага 2800 кг/м3",
-			M: 'Міцність при стисканні 142 МРа',
-			Z: 'Зносостійкість 0,60 г/см',
-			W: 'Водопоглинання 0,21%',
 		},
 	],
 };
